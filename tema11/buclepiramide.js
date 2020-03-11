74c2dce8c5d0a58c1f22f96698d61974e1dbc9e2
Alejandro Pérez <55441505+apergon@users.noreply.github.com>
2020-03-11 10:32:29 +0100

    Create buclepiramide.js

---
+++ tema11/buclepiramide.js
@@ -0,0 +1,10 @@
+<script>
+    var i, rep;
+    //BUCLE FOR PARA RECORRER DE 1 A 30
+    for (i = 1; i <= 30; i++) {
+        for (rep = 0; rep < i; rep++) {
+            document.write(i);
+        }
+        document.write("<br>");
+    }
+</script>
